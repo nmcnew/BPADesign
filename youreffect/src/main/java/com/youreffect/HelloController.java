354c46741c0ce0e516241330c356b80529af104d
Deeban Ramalingam <rdeeban@gmail.com>
2014-01-11 15:33:25 -0700

    setting up spring mvc

--- youreffect/src/main/java/com/youreffect/HelloController.java
+++ youreffect/src/main/java/com/youreffect/HelloController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		model.addAttribute("message", "Hello world!");
-		return "hello";
+        return "public/";
 	}
 }
