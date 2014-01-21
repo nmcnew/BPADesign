986a94de4d9ee7e30a4ea80d557a577e96f24e93
Nick Home Computer <themickeynick@gmail.com>
2014-01-20 17:31:53 -0700

    Got adding elements working

--- youreffect/src/main/webapp/public/assets/js/add.js
+++ youreffect/src/main/webapp/public/assets/js/add.js
@@ -1,3 +1,35 @@
-function addOption(){
-	var lightbulb = ''
+function addOptionElec() {
+	var radios = document.getElementsByName('eOptions');
+	var selected = "";
+	for (var i = 0, length = radios.length; i < length; i++) {
+		if (radios[i].checked) {
+			selected = radios[i].value;
+			break;
+		}
+	}
+	console.log(selected);
+	switch(selected) {
+		case "acCentral":
+			break;
+		case "acRoom":
+			break;
+		case "purifier":
+			break;
+		case "washer":
+			break;
+		case "dehumidifier":
+			break;
+		case "dishwasher":
+			break;
+		case "freezer":
+			break;
+		case "lightBulb":
+			$(".mainForm").append('<h3>Incandescent Bulbs</h3><div class="input-group"><input type="number" class="form-control" placeholder="Qunatity"><input type="number" class="form-control" placeholder="Average Daily Use (hours)"></div><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">75 W Incandescent <span class="caret"></span></button><ul class="dropdown-menu"><li><a href="#">40 W incandescent</a></li><li><a href="#">60 W incandescent</a></li><li><a href="#">75 W incandescent</a></li><li><a href="#">100 W incandescent</a></li><li><a href="#">150 W incandescent</a></li><li><a href="#">29 W Halogen(40 W Equivalent)</a></li><li><a href="#">43 W Halogen(60 W Equivalent)</a></li><li><a href="#">53 W Halogen(75 W Equivalent)</a></li><li><a href="#">72 W Halogen(100 W Equivalent)</a></li></ul></div></div><br/>');
+			break;
+		case "fridge":
+			break;
+		case "compactFridge":
+
+			break;
+	}
 }
