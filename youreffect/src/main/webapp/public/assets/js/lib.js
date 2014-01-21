c0f6e9bdc79d393696331f88e46fa0fcd4cc4498
Nick Home Computer <themickeynick@gmail.com>
2014-01-21 15:44:23 -0700

    changed lib.js did some other stuff

--- youreffect/src/main/webapp/public/assets/js/lib.js
+++ youreffect/src/main/webapp/public/assets/js/lib.js
@@ -1,7 +1,7 @@
 function register(username, email, password, country) {
     var user = new User(username, email, password, country);
     $.ajax({
-        url: '/user/register',
+        url:  'http://54.214.154.98:8080/user/register',
         type: 'POST',
         dataType: 'json',
         data: JSON.stringify(user),
@@ -15,7 +15,7 @@ function register(username, email, password, country) {
 function login(username, password) {
     var user = new User(username, '', password, '');
     $.ajax({
-        url: '/user/login',
+        url: 'http://54.214.154.98:8080/user/login',
         type: 'POST',
         dataType: 'json',
         data: JSON.stringify(user),
