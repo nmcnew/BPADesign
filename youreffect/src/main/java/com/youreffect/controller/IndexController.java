4679b1879314d3db6a3e3ed08bcf559a56872fdb
Deeban Ramalingam <rdeeban@gmail.com>
2014-01-23 13:31:14 -0700

    added logged in as to all index pages.

---
+++ youreffect/src/main/java/com/youreffect/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.youreffect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Deeban Ramalingam
+ * @version 0.0.9
+ * IndexController redirects web ROOT to /public/ directory
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+    /**
+     * redirects web ROOT to /public/ directory
+     * @return request URL to public
+     */
+	@RequestMapping(method = RequestMethod.GET)
+	public String index() {
+        return "redirect:/public/";
+	}
+}
