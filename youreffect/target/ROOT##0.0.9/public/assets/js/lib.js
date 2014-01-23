605b9b6ce2f096e65c17dc69fde8fba562760f4e
Deeban Ramalingam <rdeeban@gmail.com>
2014-01-22 23:20:56 -0700

    removed cross domain stuff, updated public/ with Nick's script

--- youreffect/target/ROOT##0.0.9/public/assets/js/lib.js
+++ youreffect/target/ROOT##0.0.9/public/assets/js/lib.js
@@ -1,27 +1,27 @@
 function register(username, email, password, country) {
     var user = new User(username, email, password, country);
-
     $.ajax({
-        url: 'http://localhost:8080/user/register',
+        url:  '/user/register',
         type: 'POST',
         dataType: 'json',
         data: JSON.stringify(user),
-        contentType: "application/json; charset=utf-8"
-    }).then(function(data, status, jqxhr) {
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
             console.log(data);
+        }
     });
 }
 
 function login(username, password) {
     var user = new User(username, '', password, '');
     $.ajax({
-        url: 'http://54.214.154.98:8080/user/login',
+        url: '/user/login',
         type: 'POST',
         dataType: 'json',
-        async: false,
         data: JSON.stringify(user),
-        contentType: "application/json; charset=utf-8"
-    }).then(function(data, status, jqxhr) {
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
             console.log(data);
+        }
     });
 }
