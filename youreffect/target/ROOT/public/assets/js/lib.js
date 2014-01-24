0d3e7118f1bc47e4d6bb595dae83d231b9eab0ee
Deeban Ramalingam <rdeeban@gmail.com>
2014-01-23 22:19:25 -0700

    synced

--- youreffect/target/ROOT/public/assets/js/lib.js
+++ youreffect/target/ROOT/public/assets/js/lib.js
@@ -14,31 +14,31 @@ function getContextRoot(key) {
 }
 
 function register(username, email, password, state) {
-    var user = new User(username, email, password, state);
-    $.ajax({
-        url:  getContextRoot('public') + '/user/register',
-        type: 'POST',
-        dataType: 'json',
-        data: JSON.stringify(user),
-        contentType: "application/json; charset=utf-8",
-        success: function(data) {
-            console.log(data);
-            if (data.message.toString().indexOf('successful') != -1) {
-                $("#dialog").addClass("alert-success");
-                $("#response-title").text("Success!");
+	var user = new User(username, email, password, state);
+	$.ajax({
+		url : getContextRoot('public') + '/user/register',
+		type : 'POST',
+		dataType : 'json',
+		data : JSON.stringify(user),
+		contentType : "application/json; charset=utf-8",
+		success : function(data) {
+			console.log(data);
+			if (data.message.toString().indexOf('successful') != -1) {
+				$("#dialog").addClass("alert-success");
+				$("#response-title").text("Success!");
 
-            }
-            if ($("#dialog").hasClass("hidden")) {
-                $("#dialog").removeClass("hidden");
-                $("#dialog").removeClass("alert-success");
-                $("#dialog").removeClass("alert-danger");
-                $("#dialog").addClass("alert-info");
-                $("#response-title").text("Register");
-            }
-            $("#response-text").html(data.message);
+			}
+			if ($("#dialog").hasClass("hidden")) {
+				$("#dialog").removeClass("hidden");
+				$("#dialog").removeClass("alert-success");
+				$("#dialog").removeClass("alert-danger");
+				$("#dialog").addClass("alert-info");
+				$("#response-title").text("Register");
+			}
+			$("#response-text").html(data.message);
 
-        }
-    });
+		}
+	});
 }
 
 function login(username, password) {
@@ -60,21 +60,37 @@ function login(username, password) {
                 document.getElementById("curLogin").innerHTML = (JSON
                     .parse(localStorage.getItem("curUser")).username);
             } else {
+				$("#dialog").removeClass("alert-info");
+				$("#dialog").removeClass("alert-success");
+				$("#dialog").addClass("alert-danger");
+				$("#response-title").text("Failure!");
+			}
+			if ($("#dialog").hasClass("hidden")) {
+				$("#dialog").removeClass("hidden");
+			}
+			$("#response-text").html(data.message);
 
-                $("#dialog").removeClass("alert-info");
-                $("#dialog").removeClass("alert-success");
-                $("#dialog").addClass("alert-danger");
-                $("#response-title").text("Failure!");
-            }
-            if ($("#dialog").hasClass("hidden")) {
-                $("#dialog").removeClass("hidden");
-            }
-            $("#response-text").html(data.message);
-
-        }
-    });
+		}
+	});
 }
-
+function addOptionGas() {
+	var radios = document.getElementsByName("gOptions");
+	var selected = "";
+	for (var i = 0, length = radios.length; i < length; i++) {
+		if (radios[i].checked) {
+			selected = radios[i].value;
+			break;
+		}
+	}
+	switch (selected) {
+	case "furn":
+		$(".mainForm")
+				.append(
+						'<div id="furnWrap" class="furnWrap formWrapper"><h3>Furnace</h3><!-- Remove Button --><div class="input-group removeable pull-right"><button type="button" class="btn btn-danger " onclick="removeButt($(this))"><span class="glyphicon glyphicon-minus-sign"></span> Remove</button></div><div id="fuelType" class="input-group"><div class="input-group-btn "><button type="button" class="btn btn-default dropdown-toggle "data-toggle="dropdown">Fuel <span class="caret"></span></button><ul class="dropdown-menu optionable"><li class="item">Gas</a></li><li class="item">Oil</a></li></ul></div></div><br /><div class="input-group "><input id="houseSize" type="number" class="form-control"placeholder="Size of Your House(Square Feet)"></div><br /><div id="houseEra" class="input-group"><div class="input-group-btn "><button type="button" class="btn btn-default dropdown-toggle "data-toggle="dropdown">Era of House <span class="caret"></span></button><ul class="dropdown-menu optionable" role="menu"><li>Era of House</li><li class="item">Before 1940</li><li class="item">1940-1949</li><li class="item">1950-1959</li><li class="item">1960-1969</li><li class="item">1970-1979</li><li class="item">1980-1989</li><li class="item">1990-2000</li><li class="item">2000-Present</li></ul></div></div><br /><div id="unitEra" class="input-group"><div class="input-group-btn "><button type="button" class="btn btn-default dropdown-toggle "data-toggle="dropdown">Era of Unit <span class="caret"></span></button><ul class="dropdown-menu optionable" role="menu"><li>Era of Unit</li><li class="item">1960-1969</li><li class="item">1970-1974</li><li class="item">1975-1983</li><li class="item">1984-1987</li><li class="item">1988-1991</li><li class="item">After 1992</li><li class="item">New Unit</li></ul></div></div><br /><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle "data-toggle="dropdown">Thermostat? <span class="caret"></span></button><ul class="dropdown-menu optionable"><li>Thermostat?</li><li class="item">Yes</a></li><li class="item">No</a></li></ul></div></div></div>');
+		$(".removeable").hide();
+		break;
+	}
+}
 function addOptionElec() {
 	var radios = document.getElementsByName('eOptions');
 	var selected = "";
@@ -89,7 +105,7 @@ function addOptionElec() {
 	case "acCentral":
 		$(".mainForm")
 				.append(
-						'<div class="formWrapper acCentralWrap"><h3>Central Air Conditioning</h3><div class="input-group removeable pull-right"><button type="button" onclick="removeButt(this)" class="btn btn-danger removeButt"><span class="glyphicon glyphicon-minus-sign"></span> Remove</button></div><div class="input-group "><input type="number" class="form-control"	placeholder="Number of Units"></div><br /><div class="input-group "><input type="number" class="form-control" placeholder="SEER Rating"></div><br /><div class="input-group"><div class="input-group-btn "><button type="button" class="btn btn-default dropdown-toggle " data-toggle="dropdown">Cooling Capacity (Btu/hr) <span class="caret"></span></button><ul class="dropdown-menu optionable"><li><a href="#">2.5 ton</a></li><li><a href="#">3.0 ton</a></li><li><a href="#">3.5 ton</a></li><li><a href="#">4.0 ton</a></li><li><a href="#">5.0 ton</a></li></ul></div></div><br /><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle "	data-toggle="dropdown">Thermostat? <span class="caret"></span></button><ul class="dropdown-menu optionable"><li style="color: #101010">Thermostat?</li><li><a href="#">Yes</a></li><li><a href="#">No</a></li></ul></div></div></div><br class="break"/>');
+						'<div class="accWrapper formWrapper"><h3>Central Air Conditioning</h3><div class="input-group removeable pull-right"><button type="button" class="btn btn-danger" onclick="removeButt($(this))"><span class="glyphicon glyphicon-minus-sign"></span> Remove</button></div><div class="input-group "><input type="number" class="form-control"placeholder="Number of Units"></div><br /><div class="input-group "><input type="number" class="form-control" placeholder="SEER Rating"></div><br /><div class="input-group"><div class="input-group-btn "><button type="button" class="btn btn-default dropdown-toggle "data-toggle="dropdown">Cooling Capacity (Btu/hr) <span class="caret"></span></button><ul class="dropdown-menu optionable"><li class="item">2.5 ton</a></li><li class="item">3.0 ton</a></li><li class="item">3.5 ton</a></li><li class="item">4.0 ton</a></li><li class="item">5.0 ton</a></li></ul></div></div><br /><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle "data-toggle="dropdown">Thermostat? <span class="caret"></span></button><ul class="dropdown-menu optionable"><li style="color: #101010">Thermostat?</li><li class="item">Yes</a></li><li class="item">No</a></li></ul></div></div></div>');
 		$(".removeable").hide();
 		break;
 	case "acRoom":
@@ -109,7 +125,7 @@ function addOptionElec() {
 		if ($(".mainForm").children(".bulbWrap").length == 0) {
 			$(".mainForm")
 					.append(
-							'<div class="formWrapper bulbWrap"><h3>Light Bulbs</h3>	<div class="input-group removeable pull-right"><button type="button" onclick="removeButt(this)" class="btn btn-danger"><span class="glyphicon glyphicon-minus-sign"></span> Remove</button></div><div class="input-group"><input type="number" class="form-control" placeholder="Quantity"></div><br/><div class="input-group"><input type="number" class="form-control "placeholder="Average Daily Use (hours)"></div><br/><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle " data-toggle="dropdown">75 W Incandescent <span class="caret"></span></button><ul class="dropdown-menu optionable"><li><a href="#">40 W incandescent</a></li><li><a href="#">60 W incandescent</a></li><li><a href="#">75 W incandescent</a></li><li><a href="#">100 W incandescent</a></li><li><a href="#">150 W incandescent</a></li><li><a href="#">29 W Halogen(40 W Equivalent)</a></li><li><a href="#">43 W Halogen(60 W Equivalent)</a></li><li><a href="#">53 W Halogen(75 W Equivalent)</a></li><li><a href="#">72 W Halogen(100 W Equivalent)</a></li></ul></div></div><br /></div><br class="break"/>');
+							'<div class="lBulbCover formWrapper"><h3>Light Bulbs</h3><div class="input-group removeable pull-right"><button type="button" class="btn btn-danger" onclick="removeButt($(this))"><span class="glyphicon glyphicon-minus-sign"></span> Remove</button></div><div class="input-group"><input type="number" class="form-control" placeholder="Quantity"></div><br /><div class="input-group"><input type="number" class="form-control "placeholder="Average Daily Use (hours)"></div><br /><div class="input-group"><div class="input-group-btn"><button type="button" class="btn btn-default dropdown-toggle "data-toggle="dropdown">75 W Incandescent <span class="caret"></span></button><ul class="dropdown-menu optionable"><li class="item">40 W incandescent</a></li><li class="item">60 W incandescent</a></li><li class="item">75 W incandescent</a></li><li class="item">100 W incandescent</a></li><li class="item">150 W incandescent</a></li><li class="item">29 W Halogen(40 W Equivalent)</a></li><li class="item">43 W Halogen(60 W Equivalent)</a></li><li class="item">53 W Halogen(75 W Equivalent)</a></li><li class="item">72 W Halogen(100 W Equivalent)</a></li></ul></div></div></div>');
 			$(".removeable").hide();
 		}
 
@@ -122,8 +138,8 @@ function addOptionElec() {
 	}
 }
 function removeButt(domElement) {
-	$(domElement).parents(".formWrapper").siblings(".break").remove();
 	$(domElement).parents(".formWrapper").fadeOut(300, function() {
-		$(this).remove;
+
+		$(domElement).parents(".formWrapper").remove();
 	});
 }
